ef1be912f806b010c670e6ccaec0313083f80ae0
YzakV <isaac.martinez@lakmisystems.com.mx>
2019-08-30 14:59:20 -0500

    Updates

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+
+
+
+let fecha = ["lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
+let obj = {
+   a: 7,
+   b: 2,
+   c: 3,
+   d: 4
+};
+console.log(Object);
+console.log(Object.values(obj));
+console.log(obj.lenght);
+// for(let i in fecha){
+//    console.log(i);
+   
+// }
+// for(let i = 0; i < fecha.length; i++){
+//    console.log(fecha[i]);
+// }
+
+
